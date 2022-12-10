8712eebf687a6bbe1390ecf747be6ad6aecd82ce
rayhas2work <73216882+rayhas2work@users.noreply.github.com>
2022-12-10 11:15:10 -0500

    Final Touch

--- README.md
+++ README.md
@@ -1,22 +1,70 @@
-# freestyle_project
-
-Steam Game Finder
+# Steam Game Price Comparison
 This web application allows users to find the cheapest available price for a game on Steam. The user simply enters the game's app ID, and the app will search through all available Steam regions to find the lowest price and currency. The result is displayed on the page in US dollars.
 
-Running the app
-To run the app, follow these steps:
+## Notes
+The app uses the Steam API to get information about available games and their prices in different regions.
+The app uses the AlphaVantage API to convert prices to US dollars. You will need to provide your own API key in order for this feature to work:
 
-Clone the repository: git clone https://github.com/<your-username>/steam-game-finder.git
-Install the required dependencies: pip install -r requirements.txt
-Set the FLASK_APP environment variable: export FLASK_APP=main.py
-Start the app: flask run
-The app will now be running at http://127.0.0.1:5000/.
+## Setup
+Create and activate a virtual environment:
 
-Using the app
+```sh
+conda create -n SteamPC-env python=3.8
+
+conda activate SteamPC-env
+```
+Install package dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+## Configuration
+
+[Obtain an API Key](https://www.alphavantage.co/support/#api-key) from AlphaVantage.
+
+Then create a local ".env" file and provide the key like this:
+
+```sh
+# this is the ".env" file...
+
+API_KEY="_________"
+```
+## Usage
+Run the Price Comparison App:
+
+```sh
+python app/project.py
+```
+
+```sh
+# or pass  env var from command to run
+API_KEY="_____" python app/project.py
+```
+### Web App
+
+Run the web app (then view in the browser at http://localhost:5000/):
+
 To use the app, simply go to the homepage and enter the app ID of the game you want to find. The app will search through all available Steam regions and display the lowest price in US dollars.
 
-Notes
-The app uses the Steam API to get information about available games and their prices in different regions.
-The app uses the AlphaVantage API to convert prices to US dollars. You will need to provide your own API key in order for this feature to work.
-License
-This project is licensed under the MIT License. See LICENSE for details.
+```sh
+# Mac OS:
+FLASK_APP=web_app flask run
+
+# Windows OS:
+# ... if `export` doesn't work for you, try `set` instead
+# ... or set FLASK_APP variable via ".env" file
+export FLASK_APP=web_app
+flask run
+```
+
+## Testing
+Run tests:
+
+```sh
+pytest
+```
+
+
+
+## License
+This project is licensed under the MIT License.
--- web_app/routes/project_routes.py
+++ web_app/routes/project_routes.py
@@ -1,53 +1,145 @@
-from flask import Flask, render_template
+from asyncio import log
+import logging
+from exceptiongroup import catch
+from flask import Blueprint, jsonify, request, render_template
+import flask
+from pandas import read_csv
 
-app = Flask(__name__)
+import requests
+import json
+import urllib
 
-@app.route("/game/<int:app_id>")
-def show_game(app_id):
-    # Download the CSV file if it doesn't already exist
+import os
+from dotenv import load_dotenv
+import urllib3
+
+from app.api import API_KEY
+
+load_dotenv()
+
+
+urllib3.disable_warnings()
+
+logger = logging.getLogger()
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+@home_routes.route("/home")
+def index():
+    print("HOME...")
+    #return "Welcome Home"
+    return render_template("home.html")
+
+
+
+def conversion(lowest_currency,lowest_price):
+    with urllib.request.urlopen(f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={lowest_currency}&to_currency=USD&apikey={API_KEY}') as url:
+        data3 = json.load(url)
+        if "Realtime Currency Exchange Rate" in data3:
+            rt = float(data3["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+            converted_price = rt * lowest_price
+            return converted_price
+        else:
+            # print("Exchange Rate does not exist")
+            return None
+
+def show_game_func(app_id_int):
+    app_id = "%d" % app_id_int
+    logger.error(app_id)
+
+    isInvalid = True
+    lowest_price=1000000
+    lowest_currency="none"
+
+
+    # isInvalid = False
+    # lowest_price=999.23
+    # lowest_currency = "CNY"
+
+    # return  isInvalid, lowest_currency, lowest_price
+    
+
     url = "https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv"
-
-    # Read the CSV file into a DataFrame
     country_codes_df = read_csv(url)
     country_codes = country_codes_df.to_dict("records")
 
-    # Define a function to convert prices to USD
-    def conversion(lowest_currency,lowest_price):
-        with urllib.request.urlopen(f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={lowest_currency}&to_currency=USD&apikey={API_KEY}') as url:
-            data3 = json.load(url)
-            if "Realtime Currency Exchange Rate" in data3:
-                rt = float(data3["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
-                converted_price = rt * lowest_price
-                return converted_price
-            else:
-                return None
+    all_codes=[]
+    app_list=[]
 
-    # Get the list of all country codes
-    all_codes = [str(c["alpha-2"]) for c in country_codes]
+    for c in country_codes:
+        all_codes.append(str(c["alpha-2"])) 
 
-    # Get the list of all app IDs
-    app_list = []
-    request_url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
-    response = requests.get(request_url)
+    #print(all_codes)
+
+    request_url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"  
+    response = requests.get(request_url, verify=False)
     data = json.loads(response.text)
+
     for d in data["applist"]["apps"]:
         app_list.append(str(d["appid"]))
 
-    # Check if the app ID is valid
-    if app_id not in app_list:
-        return "Invalid app ID"
+    _app_id_flag = False
+    for _app_id in app_list:
+        if _app_id == app_id:
+            _app_id_flag = True
+            break
+    if _app_id_flag != True:
+        return isInvalid, lowest_currency, lowest_price
+    #print(app_list)
 
-    # Find the cheapest price for the game
-    lowest_price = 1000000
-    lowest_currency = "none"
+
+    location="none"
+
     for code in all_codes:
-        request_url = f"http://store.steampowered.com/api/appdetails?appids={app_id}&cc={code}&filters=price_overview"
-        response = requests.get(request_url)
+        request_url=f"http://store.steampowered.com/api/appdetails?appids={app_id}&cc={code}&filters=price_overview"
+        response = requests.get(request_url, verify=False)
         data2 = json.loads(response.text)
         if data2 is not None:
-            if data2[app_id]["success"] == False:
-                continue
+            if data2[app_id]["success"]==False:
+                lowest_price=lowest_price
+                lowest_currency=lowest_currency
             else:
-                if len(data2[app_id]["data"]) == 0:
-                    return "This game is yet to be released. Please check later."
-               
+                if len(data2[app_id]["data"])==0:
+                    # print("This game is yet to be released. Please check later.")
+                    isInvalid = False
+                    return isInvalid, lowest_currency, lowest_price
+                    break
+                else:
+                    price = data2[app_id]["data"]["price_overview"]["final"]/100
+                    currency = data2[app_id]["data"]["price_overview"]["currency"]
+                    price_in_usd=conversion(currency,price)
+                    if price_in_usd is None:
+                        continue
+                    elif price_in_usd < lowest_price:
+                        lowest_price = price_in_usd
+                        lowest_currency = currency
+
+    if lowest_price != 1000000:
+        # print("Your selected game is cheapest in",lowest_currency,".")
+        # print("The lowest price in US dollar value is $%.2f."%(lowest_price))
+        # OK
+        isInvalid = False
+        return isInvalid, lowest_currency, lowest_price
+    # not find
+    isInvalid = False
+    return isInvalid, lowest_currency, lowest_price
+
+
+@home_routes.route("/game/<int:app_id>",  methods=["POST","GET"])
+def show_game(app_id):
+    try:
+        not_purchase = True
+        isInvalid, lowest_currency, lowest_price = show_game_func(app_id)
+        if lowest_price != 1000000:
+            not_purchase = False
+
+        _lowest_price = "%.2f" % lowest_price
+        return render_template("result.html", isInvalid=isInvalid, lowest_currency=lowest_currency, lowest_price=_lowest_price, not_purchase=not_purchase)
+        pass
+    except Exception as err:
+        logger.error(err)
+        return render_template("error.html")
+        pass
+
+    pass
